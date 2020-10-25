da7f8208f0c538fdd268df468b24a741619afa9b
Mateusz <sekunda.mateusz@gmail.com>
2020-10-25 17:27:32 +0100

    Add tests

--- frontend/src/main/angular/src/app/transactions-table/transactions-table.component.spec.ts
+++ frontend/src/main/angular/src/app/transactions-table/transactions-table.component.spec.ts
@@ -1,11 +1,30 @@
+import { TransactionsTableService } from './transactions-table.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { TransactionsTableComponent } from './transactions-table.component';
+import { of } from 'rxjs';
 
-describe('TransactionsTableComponent', () => {
+fdescribe('TransactionsTableComponent', () => {
   let component: TransactionsTableComponent;
   let fixture: ComponentFixture<TransactionsTableComponent>;
+
+  const transactions = [
+    {
+      id: 1,
+      title: 'test',
+      transactionDate: '2020-10-11',
+      pricePln: 111,
+      priceEur: 25
+    },
+    {
+      id: 2,
+      title: 'test 2',
+      transactionDate: '2020-10-11',
+      pricePln: 211,
+      priceEur: 55
+    }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,4 +43,24 @@ describe('TransactionsTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get transactions count',
+    inject([TransactionsTableService], (transactionsTableService: TransactionsTableService) => {
+      spyOn(transactionsTableService, 'getTransactionCount').and.returnValue(of(11));
+      component.transactionCount$.subscribe(count => {
+        expect(count).toEqual(11);
+      });
+      component.ngOnInit();
+    }));
+
+  it('should get transactions',
+    inject([TransactionsTableService], (transactionsTableService: TransactionsTableService) => {
+      spyOn(transactionsTableService, 'getTransactions').and.returnValue(of(transactions));
+
+      component.onQueryParamsChange({ pageIndex: 1, pageSize: 10, sort: null, filter: null});
+      
+      component.transactions$.subscribe(value => {
+        expect(value).toEqual(transactions);
+      });
+    }));
 });
--- frontend/src/main/angular/src/app/transactions-table/transactions-table.component.ts
+++ frontend/src/main/angular/src/app/transactions-table/transactions-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TransactionsTableService, Transaction } from './transactions-table.service';
 import { NzTableQueryParams } from 'ng-zorro-antd/table';
-import { ReplaySubject, of } from 'rxjs';
+import { ReplaySubject, of, Observable } from 'rxjs';
 import { takeUntil, tap, switchMap, finalize } from 'rxjs/operators';
 
 @Component({
@@ -12,7 +12,7 @@ import { takeUntil, tap, switchMap, finalize } from 'rxjs/operators';
 export class TransactionsTableComponent implements OnInit, OnDestroy {
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   transactionCount$ = of(0);
-  transactions: Transaction[] = [];
+  transactions$: Observable<Transaction[]> = of([]);
   loading = true;
   pageSize = 10;
   pageIndex = 1;
@@ -20,14 +20,11 @@ export class TransactionsTableComponent implements OnInit, OnDestroy {
   constructor(private transactionsTableService: TransactionsTableService) {}
 
   ngOnInit(): void {
-    this.transactionsTableService.getUpdateTransactions()
+    this.transactionCount$ = this.transactionsTableService.getUpdateTransactions()
     .pipe(
-      takeUntil(this.destroyed$)
-    )
-    .subscribe(() => {
-      this.loadDataFromServer(this.pageIndex, this.pageSize);
-      this.transactionCount$ = this.transactionsTableService.getTransactionCount();
-    });
+      takeUntil(this.destroyed$),
+      switchMap(() => this.transactionsTableService.getTransactionCount())
+    );
   }
 
   ngOnDestroy(): void {
@@ -35,19 +32,25 @@ export class TransactionsTableComponent implements OnInit, OnDestroy {
     this.destroyed$.complete();
   }
 
-  loadDataFromServer(pageIndex: number, pageSize: number): void {
+  getTransactions(pageIndex: number, pageSize: number) {
+    return this.transactionsTableService.getUpdateTransactions()
+    .pipe(
+      takeUntil(this.destroyed$),
+      switchMap(() => this.loadDataFromServer(pageIndex, pageSize))
+    );
+  }
+
+  loadDataFromServer(pageIndex: number, pageSize: number) {
     this.loading = true;
-    this.transactionsTableService.getTransactions(pageIndex, pageSize)
+    return this.transactionsTableService.getTransactions(pageIndex, pageSize)
     .pipe(
       takeUntil(this.destroyed$),
       finalize(() => this.loading = false)
-    )
-    .subscribe(data => this.transactions = data);
+    );
   }
 
-
   onQueryParamsChange({ pageSize, pageIndex }: NzTableQueryParams): void {
-    this.loadDataFromServer(pageIndex, pageSize);
+    this.transactions$ = this.getTransactions(pageIndex, pageSize);
   }
 
 }
